a785b4a6ce431edd441c442a9f76d93c9a58890f
circlelychen <truecirclely@gmail.com>
2019-10-21 01:50:03 +0800

    add test cases for ckip tokenizer

--- tests/test_ckiptokenizer.py
+++ tests/test_ckiptokenizer.py
@@ -14,27 +14,10 @@ class CKIPTokenizerTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_tokenizer_s1(self):
-        expected_resp_dict = {
-            "tokens": [
-                {'word': '昨天', 'tag': 'Nd'},
-                {'word': '晚上', 'tag': 'Nd'},
-                {'word': '吃', 'tag': 'VC'},
-                {'word': '了', 'tag': 'Di'},
-                {'word': '牛肉', 'tag': 'Na'},
-                {'word': '燴飯', 'tag': 'Na'},
-                {'word': '花', 'tag': 'VC'},
-                {'word': '了', 'tag': 'Di'},
-                {'word': '120', 'tag': 'Neu'},
-                {'word': '元', 'tag': 'Nf'}
-            ]
-        }
-
+    def test_tokenizer(self):
         tokens = self._ckip_tokenizer.tokenize("昨天晚上吃了牛肉燴飯花了120元")
         assert [t.text for t in tokens] == \
                ['昨天', '晚上', '吃', '了', '牛肉', '燴飯', '花', '了', '120',
                 '元']
         assert [t.offset for t in tokens] == \
                [0, 2, 4, 5, 6, 8, 10, 11, 12, 15]
-        # assert [t.get("ckip_pos_tag") for t in tokens] == \
-        #        ['Nd', 'Nd', 'VC', 'Di', 'Na', 'Na', 'VC', 'Di', 'Neu', 'Nf']
