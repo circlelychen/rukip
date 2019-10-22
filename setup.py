bf6a537bc2873d2fd9eed8dc02ae41f0f0f81a32
circlelychen <truecirclely@gmail.com>
2019-10-22 19:23:11 +0800

    tag 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rukip",
-    version="0.0.2",
+    version="0.0.3",
     author="Hao-Yuan Chen",
     author_email="truecirclely@gmail.com",
     description="An Embedded CKIP Rasa NLU Components",
